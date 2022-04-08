7770d2aeaf6dc03379bf05903852e35179b21f5c
jonasnys@gmail.com <jonasnys@gmail.com>
2022-04-08 16:35:52 +0200

    First fork commit

--- src/test/Main.hx
+++ src/test/Main.hx
@@ -1,17 +1,18 @@
-
 import buddy.*;
+
 using buddy.Should;
 
 @colorize
 class Main implements Buddy<[
-    TestParser,
-    tables.GposTable,
-    tables.LocaTable,
-    tables.LtagTable,
-    tables.NameTable,
-    Table,
-    TestLayout,
-    TestOpenType,
-    TestTypes,
-    utils.TestGlyphMetrics
+	//
+	TestParser,
+	tables.GposTable,
+	tables.LocaTable,
+	tables.LtagTable,
+	tables.NameTable,
+	Table,
+	TestLayout,
+	TestOpenType,
+	TestTypes,
+	utils.TestGlyphMetrics
 ]> {}
