8eda5906afb5ffaf09f423aabee7f89f08d987cb
jonasnys@gmail.com <jonasnys@gmail.com>
2022-04-14 23:42:31 +0200

    completed TestTokenizer tests

--- src/opentype/Tokenizer.hx
+++ src/opentype/Tokenizer.hx
@@ -1,38 +1,351 @@
 package opentype;
 
+import haxe.ds.StringMap;
+
+using Lambda;
+
 class Tokenizer {
-	public function new() {
+	public function new(?events:TokenizerEventMethods) {
 		this.tokens = [];
+		this.registeredContexts = new StringMap();
+		this.contextCheckers = [];
+		this.events = {};
+		this.registeredModifiers = [];
+		initializeCoreEvents(events);
 	}
 
-	var tokens:Array<Token>;
+	public var tokens(default, null):Array<Token>;
 
-	public function registerModifier(modifierId:String, condition:Dynamic, modifier:Dynamic) {}
+	var events:TokenizerEvents;
+	var registeredModifiers:Array<String>;
+
+	public final contextCheckers:Array<ContextChecker>;
+	public final registeredContexts:StringMap<ContextChecker>;
 
 	public function tokenize(text:String):Array<Token> {
 		this.tokens = [];
 		this.resetContextsRanges();
 		final chars = text.split('');
-		trace(chars);
+
+		this.dispatch('start');
+
 		for (i => char in chars) {
 			final contextParams = new ContextParams(chars, i);
 			// trace(contextParams);
+			this.dispatch('next', [contextParams]);
+
 			this.runContextCheck(contextParams);
+
 			final token = new Token(char);
 			this.tokens.push(token);
+			this.dispatch('newToken', [token, contextParams]);
 		}
-
+		this.dispatch('end', [this.tokens]);
 		return this.tokens;
 	}
 
 	function resetContextsRanges() {
 		// trace('resetContextsRanges...');
+		final registeredContexts = this.registeredContexts;
+		for (contextName => context in registeredContexts) {
+			context.ranges = [];
+		}
 	}
 
 	function runContextCheck(contextParams:ContextParams) {
 		final index = contextParams.index;
-		// trace('runContextCheck...');
+		for (contextChecker in this.contextCheckers) {
+			final contextName = contextChecker.contextName;
+			final context:ContextChecker = this.getContext(contextName);
+			var openRange = context.openRange;
+			if (openRange == null && contextChecker.checkStart(contextParams)) {
+				openRange = new ContextRange(index, null, contextName);
+				this.getContext(contextName).openRange = openRange;
+				this.dispatch('contextStart', [contextName, index]);
+			}
+
+			if (openRange != null && contextChecker.checkEnd(contextParams)) {
+				final offset = (index - openRange.startIndex) + 1;
+				final range = this.setEndOffset(offset, contextName);
+				this.dispatch('contextEnd', [contextName, range]);
+			}
+		}
 	}
+
+	function setEndOffset(offset:Int, contextName:String) {
+		final startIndex = this.getContext(contextName).openRange.startIndex;
+		var range:ContextRange = new ContextRange(startIndex, offset, contextName);
+		final ranges = this.getContext(contextName).ranges;
+		range.rangeId = '${contextName}.${ranges.length}';
+		ranges.push(range);
+		this.getContext(contextName).openRange = null;
+		return range;
+	}
+
+	static final coreEvents = [
+		//
+		'start',
+		'end',
+		'next',
+		'newToken',
+		'contextStart',
+		'contextEnd',
+		'insertToken',
+		'removeToken',
+		'removeRange',
+		'replaceToken',
+		'replaceRange',
+		// 'composeRUD',
+		'updateContextsRanges'
+	];
+
+	static final requiresContextUpdate = [
+		//
+		'insertToken',
+		'removeToken',
+		'removeRange',
+		'replaceToken',
+		'replaceRange',
+		// 'composeRUD'
+	];
+
+	function initializeCoreEvents(eventMethods:TokenizerEventMethods) {
+		// setup coreEvents
+		coreEvents.iter(eventId -> {
+			Reflect.setField(this.events, eventId, new Event(eventId));
+		});
+
+		final eventFields = Reflect.fields(eventMethods);
+		eventFields.iter(eventId -> {
+			final fn:Dynamic->Void = Reflect.field(eventMethods, eventId);
+			final event = Reflect.field(this.events, eventId);
+			event.subscribe(fn);
+		});
+
+		requiresContextUpdate.iter(eventId -> {
+			final event = Reflect.field(this.events, eventId);
+			event.subscribe(this.updateContextsRanges);
+		});
+	}
+
+	function updateContextsRanges() {
+		this.resetContextsRanges();
+		final chars = this.tokens.map(token -> token.char);
+		for (i in 0...chars.length) {
+			final contextParams:ContextParams = new ContextParams(chars, i);
+			this.runContextCheck(contextParams);
+		}
+		this.dispatch('updateContextsRanges', [this.registeredContexts]);
+	}
+
+	function dispatch(eventId:String, args:Array<Dynamic> = null) {
+		args = args == null ? [] : args;
+		final event:{subscribers:Array<Dynamic>} = Reflect.field(this.events, eventId);
+		if (event == null) {
+			throw 'Event ${eventId} does not exist';
+			return;
+		}
+
+		event.subscribers.iter((subscriber:Dynamic->Void) -> {
+			Reflect.callMethod(null, subscriber, args);
+		});
+	}
+
+	public function registerModifier(modifierId:String, condition:Dynamic, modifier:Dynamic) {
+		final event:Event<Token->ContextParams->Void> = this.events.newToken;
+
+		event.subscribe((token:Token, contextParams:ContextParams) -> {
+			final conditionParams:Array<Dynamic> = [token, contextParams];
+			final conditionIsNull:Bool = condition == null;
+			final conditionApply:Bool = Reflect.callMethod(null, condition, conditionParams);
+			final canApplyModifier = conditionIsNull || conditionApply;
+			if (canApplyModifier) {
+				// final modifierParams:Array<Dynamic> = [token, contextParams];
+				final newStateValue:Dynamic = Reflect.callMethod(null, modifier, conditionParams);
+				token.setState(modifierId, newStateValue);
+			}
+		});
+
+		this.registeredModifiers.push(modifierId);
+	}
+
+	public function registerContextChecker(contextName:String, contextStartCheck:ContextParams->Bool, contextEndCheck:ContextParams->Bool) {
+		if (this.getContext(contextName) != null) {
+			trace('context name ${contextName} is already registered');
+			return null;
+		}
+
+		final contextCheckers = new ContextChecker(contextName, contextStartCheck, contextEndCheck);
+		this.registeredContexts.set(contextName, contextCheckers);
+		this.contextCheckers.push(contextCheckers);
+		return contextCheckers;
+	}
+
+	function getContext(contextName:String):ContextChecker {
+		return this.registeredContexts.get(contextName);
+	}
+
+	public function getContextRanges(contextName:String) {
+		final context = this.getContext(contextName);
+		if (context != null)
+			return context.ranges;
+
+		trace('context checker ${contextName} is not registered.');
+		return null;
+	}
+
+	public function getRangeTokens(range:ContextRange) {
+		final endIndex = range.startIndex + range.endOffset;
+		return [].concat(this.tokens.slice(range.startIndex, endIndex));
+	}
+
+	public function insertToken(tokens:Array<Token>, index:Int, silent:Bool = false) {
+		for (idx => token in tokens)
+			this.tokens.insert(index + idx, token);
+		if (!silent) {
+			final args:Array<Dynamic> = [tokens, index];
+			this.dispatch('insertToken', []); // args missing here!
+		}
+		return tokens;
+	}
+
+	public function removeToken(index:Int, silent:Bool = false) {
+		if (!this.inboundIndex(index)) {
+			trace('removeToken: invalid token index.');
+			return null;
+		}
+		final token = this.tokens.splice(index, 1);
+		if (!silent)
+			this.dispatch('removeToken', []); // args missing here!
+		return token;
+	}
+
+	function inboundIndex(index:Int):Bool
+		return index >= 0 && index < this.tokens.length;
+
+	public function removeRange(startIndex:Int, offset:Int = null, silent:Bool = false) {
+		offset = offset != null ? offset : this.tokens.length;
+		final tokens = this.tokens.splice(startIndex, offset);
+		if (!silent)
+			this.dispatch('removeRange', []); // args missing here!
+		return tokens;
+	}
+
+	public function replaceToken(index:Int, token:Token, silent:Bool = false) {
+		if (!this.inboundIndex(index)) {
+			trace('removeToken: invalid token index.');
+			return null;
+		}
+		final replaced = this.tokens.splice(index, 1);
+		this.tokens.insert(index, token);
+		if (!silent)
+			this.dispatch('replaceToken', []); // args missing here!
+		return [replaced[0], token];
+	}
+
+	public function replaceRange(startIndex:Int, offset:Int, tokens:Array<Token>, silent:Bool = false):Array<Array<Token>> {
+		offset = offset != null ? offset : this.tokens.length;
+		if (!this.inboundIndex(startIndex)) {
+			trace('replaceRange: invalid token index.');
+			return null;
+		}
+		final replaced = this.tokens.splice(startIndex, tokens.length);
+		for (idx => token in tokens)
+			this.tokens.insert(startIndex + idx, token);
+
+		if (!silent)
+			this.dispatch('replaceToken', []); // args missing here!
+
+		return [replaced, tokens];
+	}
+
+	public function composeRUD(RUDs:Array<Array<Dynamic>>) {
+		final silent = true;
+		for (pair in RUDs) {
+			final type:String = pair[0];
+			switch type {
+				case 'insertToken':
+					final tokens:Array<Token> = cast pair[1];
+					final index:Int = cast pair[2];
+					this.insertToken(tokens, index);
+				case 'replaceToken':
+					final index:Int = cast pair[1];
+					final token:Token = cast pair[2];
+					this.replaceToken(index, token);
+				case 'replaceRange':
+					final startIndex:Int = cast pair[1];
+					final offset:Int = cast pair[2];
+					final tokens:Array<Token> = cast pair[3];
+					this.replaceRange(startIndex, offset, tokens);
+				case 'removeToken':
+					final index:Int = cast pair[1];
+					this.removeToken(index);
+				default:
+					trace(type.toUpperCase());
+			}
+		}
+	}
+}
+
+class ContextRange {
+	public function new(startIndex:Int, endOffset:Int, contextName:String) {
+		this.contextName = contextName;
+		this.startIndex = startIndex;
+		this.endOffset = endOffset;
+		this.rangeId = null;
+	}
+
+	public final contextName:String;
+	public final startIndex:Int;
+	public final endOffset:Int;
+
+	public var rangeId(default, default):String;
+}
+
+class ContextChecker {
+	public function new(contextName:String, checkStart:ContextParams->Bool, checkEnd:ContextParams->Bool) {
+		this.contextName = contextName;
+		this.openRange = null;
+		this.ranges = [];
+		this.checkStart = checkStart;
+		this.checkEnd = checkEnd;
+	}
+
+	public final contextName:String;
+	public final checkStart:ContextParams->Bool;
+	public final checkEnd:ContextParams->Bool;
+
+	public var openRange(default, default):ContextRange;
+	public var ranges(default, default):Array<ContextRange>;
+}
+
+typedef TokenizerEventMethods = {
+	?start:Void->Void,
+	?next:ContextParams->Void,
+	?end:Array<Token>->Void,
+	?newToken:Token->ContextParams->Void,
+	?contextStart:String->Dynamic->Void,
+	?contextEnd:String->Dynamic->Void,
+}
+
+typedef TokenizerEvents = {
+	?start:Event<Void->Void>,
+	?next:Event<ContextParams->Void>,
+	?end:Event<Array<Token>->Void>,
+	?newToken:Event<Token->ContextParams->Void>,
+}
+
+class Event<EMethod> {
+	public function new(eventId:String) {
+		this.eventId = eventId;
+		this.subscribers = [];
+	}
+
+	public final eventId:String;
+	public final subscribers:Array<EMethod>;
+
+	public function subscribe(fn:EMethod)
+		this.subscribers.push(fn);
 }
 
 class ContextParams {
@@ -48,9 +361,20 @@ class ContextParams {
 	public final context:Array<String>;
 	public final index:Int;
 	public final length:Int;
-	public final current:Dynamic;
+	public final current:String;
 	public final backtrack:Array<String>;
 	public final lookahead:Array<String>;
+
+	public function get(offset:Int):String {
+		if (offset == 0) {
+			return this.current;
+		} else if (offset < 0 && Math.abs(offset) <= this.backtrack.length) {
+			return this.backtrack.slice(offset)[0];
+		} else if (offset > 0 && offset <= this.lookahead.length) {
+			return this.lookahead[offset - 1];
+		}
+		return null;
+	}
 }
 
 class Token {
@@ -62,6 +386,10 @@ class Token {
 
 	public final char:String;
 	public final state:Dynamic;
+	public final activeState:Dynamic;
 
-	final activeState:Null<Bool>;
+	public function setState(key:String, value:Dynamic) {
+		Reflect.setField(this.state, key, value);
+		Reflect.setField(this, 'activeState', {key: key, value: value});
+	}
 }
--- src/test/TestTokenizer.hx
+++ src/test/TestTokenizer.hx
@@ -3,19 +3,58 @@ import opentype.Tokenizer;
 
 using buddy.Should;
 using StringTools;
+using opentype.utils.CharUtils;
+using Lambda;
 
 class TestTokenizer extends buddy.BuddySuite {
 	public function new() {
-		describe('TestTokenizer', {
+		describe('tokenize', {
 			var tokenizer:Tokenizer;
 			beforeAll({
 				tokenizer = new Tokenizer();
 			});
 			it('should map some text to a list of tokens', function() {
 				final tokens = tokenizer.tokenize('AB');
-				trace(tokens);
 				tokens[0].char.should.be('A');
 				tokens[1].char.should.be('B');
+			});
+		});
+
+		describe('tokenizer events', {
+			it('should dispatch events start, next, newToken and end', {
+				var start = false;
+				var end = false;
+				var chars = '';
+				var tokens = [];
+				final tokenizerEvents = new Tokenizer({
+					start: () -> {
+						// trace('Helooooooo from START event');
+						start = true;
+					},
+					next: (contextParams:ContextParams) -> {
+						// trace('Hellllooo from NEXT event');
+						chars += contextParams.current;
+					},
+					end: (args:Dynamic) -> {
+						// trace('Helllooo from END event');
+						end = true;
+					},
+					newToken: (token, contextParams) -> tokens.push(token),
+				});
+				tokenizerEvents.tokenize('AB');
+				start.should.be(true);
+				end.should.be(true);
+				chars.should.be('AB');
+				tokens.length.should.be(2);
+			});
+		});
+
+		describe('Test unicode', {
+			it('try unicode charCodeAt for icons', {
+				final icons = '☃★♲';
+				icons.charCodeAt(0).should.be(9731);
+				icons.charCodeAt(1).should.be(9733);
+				icons.charCodeAt(2).should.be(9842);
 			});
 		});
 
@@ -25,17 +64,204 @@ class TestTokenizer extends buddy.BuddySuite {
 				tokenizer = new Tokenizer();
 			});
 			it('should register and apply a state modifier to a token or more', {
-				final isChar = (token:Token) -> {
+				// condition method
+				final isChar:Token->ContextParams->Bool = (token:Token, contextParams:ContextParams) -> {
 					final char = token.char;
-					final isC = (char != null && char.length == 1);
-					trace('run isChar ' + token + ' ' + isC);
+					final isC:Bool = (char != null && char.length == 1);
+					return isC;
 				}
 
-				final charToCodePoint = (token:Token) -> token.char.charCodeAt(0);
+				// modifyer method
+				final charToCodePoint:Token->ContextParams->Dynamic = (token:Token, contextParams:ContextParams) -> token.char.charCodeAt(0);
+
 				tokenizer.registerModifier('charToCodePoint', isChar, charToCodePoint);
-				final tokens = tokenizer.tokenize('Jello World');
+
+				final tokens = tokenizer.tokenize('Jello world');
 				final jToken:Token = tokens[0];
-				trace(jToken);
+				jToken.state.charToCodePoint.should.be(74);
+				jToken.activeState.key.should.be('charToCodePoint');
+				jToken.activeState.value.should.be(74);
+			});
+		});
+
+		describe('registerContextChecker', {
+			var tokenizerContextual:Tokenizer = null;
+			var text = 'Hello World';
+			var wordStartIndexes = [];
+			var wordEndOffsets = [];
+			beforeAll({
+				final onContextStart = (contextName, startIndex) -> {
+					if (contextName == 'word')
+						wordStartIndexes.push(startIndex);
+				};
+
+				final onContextEnd = (contextName, range) -> {
+					if (contextName == 'word')
+						wordEndOffsets.push(range.endOffset);
+				};
+
+				tokenizerContextual = new Tokenizer({
+					contextStart: onContextStart,
+					contextEnd: onContextEnd
+				});
+
+				function wordStartCheck(contextParams:ContextParams):Bool {
+					final char = contextParams.current;
+					final prevChar = contextParams.get(-1);
+					final check = ((!char.isWhiteSpace() && prevChar.isWhiteSpace()) || prevChar == null && !char.isWhiteSpace());
+					return check;
+				}
+
+				function wordEndCheck(contextParams:ContextParams):Bool {
+					final nextChar = contextParams.get(1);
+					final check = (nextChar.isWhiteSpace() || nextChar == null);
+					return check;
+				}
+
+				function spaceStartCheck(contextParams:ContextParams) {
+					return contextParams.current.isWhiteSpace();
+				}
+
+				function spaceEndCheck(contextParams:ContextParams) {
+					return !contextParams.get(1).isWhiteSpace();
+				}
+
+				tokenizerContextual.registerContextChecker('word', wordStartCheck, wordEndCheck);
+				tokenizerContextual.registerContextChecker('whitespace', spaceStartCheck, spaceEndCheck);
+				tokenizerContextual.tokenize(text);
+			});
+
+			it('should dispatch contextual event contextStart and contextEnd', {
+				wordStartIndexes.should.containExactly([0, 6]);
+				wordEndOffsets.should.containExactly([5, 5]);
+			});
+
+			it('should retrieve found ranges of a registered context (word)', {
+				final whitespaceRanges = tokenizerContextual.getContextRanges('whitespace');
+				var whiteSpacesCount = 0;
+				whitespaceRanges.iter(range -> {
+					whiteSpacesCount += tokenizerContextual.getRangeTokens(range).length;
+				});
+				whiteSpacesCount.should.be(1);
+
+				var wordRanges = tokenizerContextual.getContextRanges('word');
+				var words = wordRanges.map(range -> {
+					final tokens = tokenizerContextual.getRangeTokens(range);
+					return tokens.map(token -> token.char).join('');
+				});
+				words.should.containExactly(['Hello', 'World']);
+			});
+		});
+
+		describe('insert, delete, and replace a token or a range of tokens', {
+			var tokens = [];
+			var tokenizer;
+			beforeEach(function() {
+				// trace('beforeEach');
+				tokenizer = new Tokenizer();
+
+				function aanStartCheck(contextParams:ContextParams) {
+					final char = contextParams.current;
+					final prevChar = contextParams.get(-1);
+					final a = (char.toLowerCase() == 'a' && (prevChar == null || prevChar.isWhiteSpace()));
+					return a;
+				}
+
+				function aanEndCheck(contextParams:ContextParams) {
+					final char = contextParams.current;
+					final nextChar = contextParams.get(1);
+					return ((char.toLowerCase() == 'a' && nextChar.isWhiteSpace()) || (char == 'n' && nextChar.isWhiteSpace()));
+				}
+				tokenizer.registerContextChecker('Aan', aanStartCheck, aanEndCheck);
+
+				// function arabicWordStartCheck(contextParams:ContextParams) {
+				// 	final char = contextParams.current;
+				// 	final prevChar = contextParams.get(-1);
+				// 	return ((char.isArabicChar() && prevChar.isWhiteSpace()) || (char.isArabicChar() && prevChar == null));
+				// }
+
+				// function arabicWordEndCheck(contextParams:ContextParams) {
+				// 	final char = contextParams.current;
+				// 	final nextChar = contextParams.get(1);
+				// 	return ((char.isArabicChar() && nextChar.isWhiteSpace()) || (char.isArabicChar() && nextChar == null));
+				// }
+				// tokenizer.registerContextChecker('arabicWord', arabicWordStartCheck, arabicWordEndCheck);
+				tokens = tokenizer.tokenize('B a voice not an echo');
+			});
+
+			function getAan() {
+				final aanRanges = tokenizer.getContextRanges('Aan');
+				final aanTokens = aanRanges.map(range -> tokenizer.getRangeTokens(range));
+				return aanTokens.map(tokens -> tokens.map(token -> token.char).join(''));
+			}
+
+			it('should insert a token or more at a specified index', {
+				tokenizer.insertToken([new Token('e')], 1);
+				final quote = tokenizer.tokens.map(t -> t.char).join('');
+				quote.should.be('Be a voice not an echo');
+				getAan().should.containExactly(['a', 'an']);
+			});
+
+			it('should delete a token at a specific index', {
+				tokenizer.removeToken(0); // [0:B] a voice not an echo
+				tokenizer.removeToken(0); // [0: ]a voice not an echo
+				final quote = tokenizer.tokens.map(t -> t.char).join('');
+				quote.should.be('a voice not an echo');
+				getAan().should.containExactly(['a', 'an']);
+			});
+
+			it('should remove a range of tokens', {
+				tokenizer.removeRange(9); // ' not an echo'
+				final quote = tokenizer.tokens.map(t -> t.char).join('');
+				quote.should.be('B a voice');
+				getAan().should.containExactly(['a']);
+			});
+
+			it('should replace a token with another token', {
+				tokenizer.replaceToken(0, new Token('ß')); // B
+				final quote = tokenizer.tokens.map(t -> t.char).join('');
+				quote.should.be('ß a voice not an echo');
+				getAan().should.containExactly(['a', 'an']);
+			});
+
+			it('should replace a range of tokens with tokens list', {
+				var rangesBefore = tokenizer.getContextRanges('Aan');
+				rangesBefore.iter(range -> {
+					var startIndex = range.startIndex;
+					var endOffset = range.endOffset;
+					var newTokens = tokenizer.getRangeTokens(range).map(r -> new Token('…'));
+					tokenizer.replaceRange(startIndex, endOffset, newTokens); // null => end
+				});
+				final quote = tokenizer.tokens.map(t -> t.char).join('');
+				quote.should.be('B … voice not …… echo');
+				getAan().should.containExactly([]);
+				// assert.equal(quote, 'B … voice not …… echo');
+				// assert.deepEqual(getAan(), [], 'make sure to update contexts ranges after insert, delete, and replace!');
+			});
+			it('should compose a set of operations', {
+				// input: 'B a voice not an echo'
+				tokenizer.composeRUD([
+					['insertToken', "Don't ".split('').map(c -> new Token(c)), 0], // 'Don't B a voice not an echo'
+					['replaceToken', 6, new Token('b')], // 'Don't b a voice not an echo'
+					['insertToken', [new Token('e')], 7], // 'Don't be a voice not an echo'
+					[
+						'replaceRange',
+						11,
+						null,
+						"follower be a student!".split('').map(c -> new Token(c))
+					], // 'Don't be a follower be a student!'
+					['removeToken', 32]
+				]);
+				final quote = tokenizer.tokens.map(t -> t.char).join('');
+				quote.should.be("Don't be a follower be a student");
+				getAan().should.containExactly(['a', 'a']);
+			});
+		});
+
+		describe('test opentype.utils.CharUtils', {
+			it('regex should match ', {
+				'a'.isLatinChar().should.be(true);
+				'å'.isLatinChar().should.be(false);
 			});
 		});
 	}
